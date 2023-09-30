b24b027bc60a80e1ca7b76e91e336e5021de3d06
Lei Yan <l.yan@epfl.ch>
2023-09-30 17:29:30 +0200

    nfos url: use https

--- utils/build-nfos.sh
+++ utils/build-nfos.sh
@@ -2,7 +2,7 @@
 
 # Install NFOS under HOME to be compatible with benchmark scripts
 pushd ${HOME}
-    git clone git@github.com:dslab-epfl/nfos.git --recurse-submodules
+    git clone https://github.com/dslab-epfl/nfos.git --recurse-submodules
     pushd nfos
         bash deps/setup-deps.sh
     popd
