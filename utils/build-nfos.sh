c6e23f2f9b075b8ff827cc4e3a010aff5b274992
maximilian1064 <lei.yan@rwth-aachen.de>
2023-11-09 04:13:22 +0100

    Update build-nfos.sh as nfos-experiments is part of nfos repo

--- utils/build-nfos.sh
+++ utils/build-nfos.sh
@@ -2,7 +2,6 @@
 
 # Install NFOS under HOME to be compatible with benchmark scripts
 pushd ${HOME}
-    git clone https://github.com/dslab-epfl/nfos.git --recurse-submodules
     pushd nfos
         bash deps/setup-deps.sh
     popd
